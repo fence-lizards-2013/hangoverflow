77f623915d835d54599a3910c1d0e12e1a09ed25
Apprentice <apprentice@dbc19.local>
2013-07-17 11:26:31 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HangOverflow::Application.config.secret_token = 'e594704378423b9d0f1cd5344b6b2927917e226ef3b616a3457530f461ca134b605be103de687cec0e676b4c6e8469db94816a5e9f0ca122ae6cc1659665f413'
