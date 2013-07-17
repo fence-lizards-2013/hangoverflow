531780e6318eb1a5c21404b1b3c24063ebd2fb5b
Apprentice <apprentice@dbc19.local>
2013-07-17 12:20:21 -0700

    Finished Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
-  
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :overage
+
+  validates :overage, acceptance: { message: "!!!  You Must Be Over 21 To Join" }
 end
