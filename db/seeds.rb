a172609ce40a8fb3346216fe0402f45f00c4a018
Jared Smith <jared.rit@me.com>
2013-07-17 19:29:19 -0700

    Finish adding seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
+# puts 'DEFAULT USERS'
+# user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+# puts 'user: ' << user.name
 
-Cocktail.create(name: "Mojito", description: "Great drink if you have mint lying around.", instructions: "Muddle mint, add all ingredients, DONT SHAKE, stir.")
-Cocktail.create(name: "Cape Fizz", description: "Kind of a girly drink.", instructions: "Put twice as much alcohol in and you won't even taste it..")
+
+Cocktail.create(user_id: '1', name: "Mojito", description: "Great drink if you have mint lying around.", instructions: "Muddle mint, add all ingredients, DONT SHAKE, stir.")
+Cocktail.create(user_id: '1', name: "Cape Fizz", description: "Kind of a girly drink.", instructions: "Put twice as much alcohol in and you won't even taste it..")
 
