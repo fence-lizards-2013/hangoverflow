1c65ce283f70150836b71e8baf1b4e9f35cffac1
Federico Gonzalez <fedegl@gmail.com>
2013-07-17 23:17:41 -0700

    created dynamic nested form, removed ajax

--- app/assets/javascripts/ingredient.js
+++ app/assets/javascripts/ingredient.js
@@ -1,20 +1,9 @@
 $(document).ready(function(){
-  console.log('document ready');
-
-  $('#add_ingredient').on('click',function(e){
-      e.preventDefault()
-      console.log('clicking on add ingredient');
-
-      var url = $(this).attr('href')
-      console.log(url)
-
-      $.ajax({
-        type: 'get',
-        url: url
-      }).done(function(data){
-        console.log("Meh")
-        $('.ingredients_wrapper').append(data);
-      });
-  });
-
+  $('form').on('click', '.add_fields', function(e){
+  var time = new Date().getTime();
+  var regex = new RegExp($(this).attr('data-id'), 'g');
+  $(this).before($(this).data('fields').replace(regex,time)); 
+  }); 
 });
+
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def link_to_add_fields(name, f, association)
+    # name = Add Ingredient, f references form builder var f, association :ingredients
+    new_object = f.object.send(association).klass.new
+    # f.object = @cocktail , send = f.object will 'send' (call) the assocition, so ingredients. summary = @cocktail.ingredients
+    # klass = class Ingredient, object you want to initialize
+    id = new_object.object_id
+    # object_id is a Ruby method to assign a unique ID to these objects
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+    # This will make the HTML id elements unique
+      render(association.to_s.singularize, f: builder)
+    #This will render a ingredient_fields, with the fields for f object
+    end
+    link_to(name, '#', class: 'add_fields', data: {id: id, fields: fields.gsub("\n","")})
+    #This will link to name (Add Ingredient) with the id and fields as part of the data attribute of the HTML anchor 
+  end
 end
