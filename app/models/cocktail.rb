53e214a6bc27bb1fe125dae311370620d333a1ed
Jared Smith <jared.rit@me.com>
2013-07-17 19:23:47 -0700

    Make user_id accessible for seeding.

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ActiveRecord::Base
-  attr_accessible :description, :instructions, :name, :ingredients_attributes
+  attr_accessible :description, :instructions, :name, :ingredients_attributes, :user_id
   has_many :ingredients
   belongs_to :user
   has_many :votes
