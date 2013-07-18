bd2b7184c7b0dc5fe406d1fa9f4748df3652776b
Jared Smith <jared.rit@me.com>
2013-07-17 19:29:00 -0700

    Add more tests to vote_spec, still pending.

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,7 +1,18 @@
 require 'spec_helper'
 
 describe "Vote" do
-	it "should update the cocktail owner's reputation after creation"
+	describe "#update_cocktail_vote_score"
+		
+		it "should update the cocktail's vote_score"
+	
+	end
+	
+	describe "#update_user_reputation" do
+
+		it "should update the cocktail owner's reputation after creation"
+
+	end
+	
 
 	it "should only ever have a value of 1 or -1"
 
