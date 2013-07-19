4e1e3cd1a490c7e991d147b6b182d10401f4249d
Jared Smith <jared.rit@me.com>
2013-07-19 15:12:05 -0700

    Final additions for voting. Added model and db level constraints to ensure only voting once. Fixed bugs in model method calls and made the appropriate controller methods as well.

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ActiveRecord::Base
-  attr_accessible :description, :instructions, :name, :ingredients_attributes, :user_id
+  attr_accessible :description, :instructions, :name, :ingredients_attributes, :user_id, :vote_score
   has_many :ingredients, :dependent => :destroy
   has_many :votes, :dependent => :destroy
   belongs_to :user
@@ -7,7 +7,7 @@ class Cocktail < ActiveRecord::Base
   accepts_nested_attributes_for :ingredients, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
 
   def update_vote_score!(value)
-  	vote_score += value
-  	vote_score.save
+  	self.vote_score += value
+  	self.save
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,11 @@
 class Vote < ActiveRecord::Base
+	attr_accessible :user_id, :cocktail_id, :value
 	belongs_to :user
 	belongs_to :cocktail
-	validates :value, :inclusion => {:in => [-1,1]}
+	validates :value, inclusion: {:in => [-1,1]}
+	validates_uniqueness_of :user_id, scope: :cocktail_id
+
+
 
 	after_save :update_cocktail_vote_score
 	after_save :update_user_reputation
@@ -9,11 +13,11 @@ class Vote < ActiveRecord::Base
 
 	private
 	def update_cocktail_vote_score
-		cocktail.update_vote_score!(value)
+		self.cocktail.update_vote_score!(self.value)
 	end
 
 	def update_user_reputation
-		user.update_reputation!
+		self.user.update_reputation!
 	end
 
 end
