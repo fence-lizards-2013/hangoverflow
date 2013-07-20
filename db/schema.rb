5f6fe5447c05a98c964a8c89cbe9dc374c8df874
Jared Smith <jared.rit@me.com>
2013-07-19 18:11:38 -0700

    Add css styling for the cocktail show page.

--- db/schema.rb
+++ db/schema.rb
@@ -27,9 +27,8 @@ ActiveRecord::Schema.define(:version => 20130719220019) do
     t.string   "instructions"
     t.integer  "user_id"
     t.integer  "votes_id"
-    t.integer  "vote_score",   :default => 0
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
     t.integer  "vote_score",   :default => 0
     t.string   "image_id"
   end
