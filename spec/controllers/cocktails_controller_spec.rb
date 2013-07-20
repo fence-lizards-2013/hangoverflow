bcd5c41ce8bb03a8c5de3c6bc608b84d0b5998c2
Apprentice <apprentice@dbc19.local>
2013-07-19 18:24:58 -0700

    still working on tests

--- spec/controllers/cocktails_controller_spec.rb
+++ spec/controllers/cocktails_controller_spec.rb
@@ -1,19 +1,33 @@
 require 'spec_helper'
 
 describe CocktailsController do
-
-  let!(:cocktail) { FactoryGirl.create(:cocktail) }
-  let!(:user) { FactoryGirl.create(:user) }
+  let!(:ingredient) {FactoryGirl.create(:ingredient)}
+  let!(:cocktail) { ingredient.cocktail }
+  let!(:user) { cocktail.user }
 
   before(:each) do
     sign_in(user)
   end
 
-  describe "GET 'show'" do
+  describe "visiting the cocktail view page" do
     it "should be successful" do
       get 'show', :id => cocktail.id
       response.should be_success
     end
+  end
+
+  describe "Searching for a cocktail" do
+    it "an ingredient menu should appear when the user clicks the search by ingredient button" do
+      visit '/'
+      expect(page).to have_content("Search by Ingredient")
+      expect(page).to_not have_content("Vodka")
+      click_button("Search by Ingredient")
+      expect(page).to have_content("Vodka")
+    end
+
+    it "should provide a list of unique possible ingredients"
+
+    it "should go to a search results page when the user clicks on the search button"
   end
 
 end
