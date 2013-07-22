3b6460c98e032668ee6fd8f8abfe239a75cf0563
Apprentice <apprentice@dbc24.local>
2013-07-21 18:05:33 -0700

    Ajaxify Done

--- app/assets/javascripts/cocktail.js
+++ app/assets/javascripts/cocktail.js
@@ -1,6 +1,5 @@
 var Cocktail = {
   init: function(){
-    this.container = $('#cocktail_grid');
     $('.delete_cocktail').on('ajax:success', this.deleteCocktail);
     $('#search_button').on('click', this.openSearchForm);
     $('#modal form').on('ajax:success', this.renderCocktails);
@@ -8,44 +7,32 @@ var Cocktail = {
     this.getCocktails();
   },
 
-  renderCocktails: function(event, data) {
-
+  renderCocktails: function(event, response) {
     console.log("Rendering Cocktails");
+      $('.search_form_wrapper').fadeOut();
+      $('#modal form')[0].reset();
 
-    $('.search_form_wrapper').hide();
-    Cocktail.container.masonry('remove', Cocktail.container.children());
-    Cocktail.container.children().remove();
-    Cocktail.container.append($(data));
-    Cocktail.container.masonry('reloadItems');
+      Cocktail.container.children().remove();
 
-    // debugger
-    // Cocktail.container.children().hide().fadeIn();
+      var fragment = document.createDocumentFragment();
+      var elems = [];
+      $.each($(response), function(index,elem) {
+        if (index%2 == 0) {
+          fragment.appendChild(elem);
+          elems.push(elem);
+        }
+      });
+
+      Cocktail.container.prepend( fragment );
+      Cocktail.mason.prepended( elems );
+
   
   },
 
   getCocktails: function() {
     $.get("/cocktails")
     .done(function(response){
-
-      Cocktail.container.append($(response));
-      Cocktail.container.children().hide().fadeIn();
-      Grid.init();
-      Cocktail.container.imagesLoaded(function(){
-        console.log('imagesLoaded');
-      });
-
-      // var interval = 0;
-      //  $(response).each(function(){
-      //     var cocktail = this;
-      //     setTimeout(function() {
-      //       $('#cocktail_grid').append(cocktail);
-      //     }, interval = interval + 100);
-      //   });
-
-      //  setTimeout(function(){
-      //    Grid.init();
-      //  }, 5000);
-       
+      Cocktail.renderCocktails("",response);
     });
   },
 
@@ -83,19 +70,16 @@ var Cocktail = {
 var Grid = {
   init: function(){
     console.log("Initializing Masonry");
-    
+    Cocktail.container = $('#cocktail_grid');
     Cocktail.container.masonry({
       columnWidth: 1,
       itemSelector: '.cocktail'
     });
-  },
-
-  destroy: function() {
-    console.log("Destroying Masonry");
-    Cocktail.container.masonry('destroy');
+    Cocktail.mason = Cocktail.container.data('masonry')
   }
 }
 
 $(document).ready(function(){
   Cocktail.init();
+  Grid.init();
 });
