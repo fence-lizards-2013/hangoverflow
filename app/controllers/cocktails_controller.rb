910564bcba80f2e404fc12f4008a14953d2d9841
Jeffrey Wescott <jeffrey.wescott@gmail.com>
2013-07-23 16:26:10 -0700

    simplify search logic

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -43,13 +43,9 @@ class CocktailsController < ApplicationController
 
   def search
     selected_ingredient_names = parse_search_form_input(params)
-
-    selected_ingredients = find_ingredient_obj_by_name(selected_ingredient_names)
-
-    cocktail_id_numbers = find_cocktail_ids_by_ingredients(selected_ingredients)
-
-    @cocktails = Cocktail.find(cocktail_id_numbers)
-    @ingredients = Ingredient.unique_ingredient_names.sort
+    @cocktails = Ingredient.where("name in (?)", selected_ingredient_names).map do |i|
+      i.cocktail
+    end.uniq
 
     render :json => render_to_string(@cocktails).to_json
   end
@@ -66,21 +62,4 @@ class CocktailsController < ApplicationController
     selected_ingredient_names
   end
 
-  def find_ingredient_obj_by_name(selected_ingredient_names)
-    selected_ingredients = []
-
-    selected_ingredient_names.each do |name|
-      selected_ingredients << Ingredient.find_all_by_name(name)
-    end
-    selected_ingredients.flatten!
-  end
-
-  def find_cocktail_ids_by_ingredients(selected_ingredients)
-    cocktail_id_numbers = []
-
-    selected_ingredients.each do |ingredient|
-      cocktail_id_numbers << ingredient.cocktail_id unless cocktail_id_numbers.include?(ingredient.cocktail_id)
-    end
-    cocktail_id_numbers
-  end
 end
